e2e1b6faa0d176a8005e1ae6979f8f9237afb635
Abhin Krishna K A <abhinkrishna.ka@gmail.com>
2021-11-18 13:56:31 +0530

    remove custom global styles

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import appTheme from './theme/material';
 import App from './App';
-import './styles/index.css';
 
 ReactDOM.render(
   <React.StrictMode>
